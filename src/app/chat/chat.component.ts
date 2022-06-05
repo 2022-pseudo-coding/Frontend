5db31283cb6d8109031e1cf6b58e505e93de40a1
LiZhenxin <1398448799@qq.com>
2022-06-05 15:06:31 +0800

    chat finished

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PlayerService } from '../services/player.service';
 
 @Component({
@@ -7,20 +7,51 @@ import { PlayerService } from '../services/player.service';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-  showSideNav:boolean = false;
+  showSideNav: boolean = false;
+  text: string = "";
+  others: any[] = [];
+  messages: any[] = [];
+  @Input() mapId!: string;
 
   constructor(private playerService: PlayerService) { }
 
   ngOnInit(): void {
-    this.playerService.onOthersSpeak().subscribe((resp:any) => {
-      //todo
-      let username = resp.username;
-      let message = resp.message;
+    this.playerService.onMyJoin().subscribe((resp: any) => {
+      this.others = resp.others;
+    });
+    this.playerService.onOthersSpeak().subscribe((resp: any) => {
+      this.pushMessage({
+        isMe: false,
+        content: resp.message,
+        username: resp.username,
+        modelName: resp.modelName
+      });
+    });
+    this.playerService.onOthersJoin().subscribe((resp: any) => {
+      this.others.push(resp);
+    });
+    this.playerService.onOthersQuit().subscribe((resp: any) => {
+      this.others = this.others.filter(value=>{
+        return value.id !== resp;
+      })
     });
   }
 
-  speak(message:string): void{
-    this.playerService.mySpeak(message);
+  speak(): void {
+    this.pushMessage({
+      isMe: true,
+      content: this.text,
+      username: localStorage.getItem('username'),
+      modelName: localStorage.getItem('modelName')
+    });
+    this.playerService.mySpeak(this.text);
+    this.text = '';
   }
 
+  pushMessage(message: any): void {
+    if (this.messages.length === 8) {
+      this.messages.shift();
+    }
+    this.messages.push(message);
+  }
 }
